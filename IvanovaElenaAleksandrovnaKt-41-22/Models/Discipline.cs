4c78e879872e5f728e84e47c72f6cf176a18da1c
Lenokk27 <iv.elena.aleks@gmail.com>
2025-05-05 21:22:37 +0300

    posle zashiti4 vetki objedinila

--- IvanovaElenaAleksandrovnaKt-41-22/Models/Discipline.cs
+++ IvanovaElenaAleksandrovnaKt-41-22/Models/Discipline.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public ICollection<Load> Loads { get; set; } = new List<Load>();
-
+        //kkk
     }
 }
